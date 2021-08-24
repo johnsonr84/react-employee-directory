09bf9a6c07eaea0e67b1ffc042f16b14b27be2c1
Your Name <robertcjohnson1984@gmail.com>
2021-08-24 14:44:28 -0600

    update searchbar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -6,19 +6,21 @@ function SearchBar({ empSearchData }) {
         empSearchData(e.target.value)
         console.log(e.target.value)
     }
-    
+
     return (
-        <div className="container">
-            <div className="form-group p-2 m-3">
-                <input
-                    type="text"
-                    onChange={handleOnChange}
-                    name="search"
-                    list="term"
-                    className="form-control"
-                    placeholder="Search Employee Name or Email"
-                    id="term"
-                />
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <div className="form-group p-2 m-3">
+                    <input
+                        type="text"
+                        onChange={handleOnChange}
+                        name="search"
+                        list="term"
+                        className="form-control"
+                        placeholder="Search Employee Name or Email"
+                        id="term"
+                    />
+                </div>
             </div>
         </div>
     );

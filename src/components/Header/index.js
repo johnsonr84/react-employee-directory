17babd09fc7ebb5500bed2eb6bbab843f5e2bf7f
Your Name <robertcjohnson1984@gmail.com>
2021-08-24 14:31:12 -0600

    styled header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,9 @@ import './style.css';
 function Header({ title }) {
 	return (
 		<header>
-			<h1>React Employee Directory</h1>
+			<div class="row bg-dark text-center p-3">
+				<h1>React Employee Directory</h1>
+			</div>
 		</header>
 	);
 }

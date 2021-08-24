9f240dd536c3f72f22fb38b74c86f9384339a461
Your Name <robertcjohnson1984@gmail.com>
2021-08-24 00:22:30 -0600

    update header title

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,12 @@
-export { default } from './Header';
+import React from 'react';
+import './style.css';
+
+function Header({ title }) {
+	return (
+		<header>
+			<h1>React Employee Directory</h1>
+		</header>
+	);
+}
+
+export default Header;

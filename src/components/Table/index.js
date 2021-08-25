b932ed270064d6fb9be148d54d1aa77dd196db6f
Your Name <robertcjohnson1984@gmail.com>
2021-08-24 23:06:52 -0600

    clean up code

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,23 +1,15 @@
 import React from "react";
 
 function Table({ name, image, phone, email }) {
-  
-    return (
-      <tr>
-        <td>
-          {name}
-        </td>
-        <td>
-          <img alt={name} src={image} />
-        </td>
-        <td>
-          {phone}
-        </td>
-        <td>
-          {email}
-        </td>
-      </tr> 
-    );
-  }
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td><img alt={name} src={image} /></td>
+      <td>{phone}</td>
+      <td>{email}</td>
+    </tr> 
+  );
+}
 
 export default Table;

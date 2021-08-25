3ce1f067a28d0c7842cef7cf5c7fe05daefb4af3
Your Name <robertcjohnson1984@gmail.com>
2021-08-24 23:32:59 -0600

    Updates

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
-export default {
-  getEmployees: function() {
-    return axios.get("https://randomuser.me/api/?nat=us&results=25");
-  }
-};
